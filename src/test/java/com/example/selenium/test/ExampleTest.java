5f92e373fc6ec743e470986d16bd8553f5280ef3
Leandro Santana <leandro.rodrigues.santana@hotmail.com>
2019-09-03 14:56:18 -0300

    Build com junit no pom

--- src/test/java/com/example/selenium/test/ExampleTest.java
+++ src/test/java/com/example/selenium/test/ExampleTest.java
@@ -1,10 +1,8 @@
 package com.example.selenium.test;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.Comparator;
 
-import org.apache.commons.io.FileUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
@@ -45,36 +43,40 @@ public class ExampleTest extends GenerateReport {
 //		}
 //	}
 	
-	private void saveFile() {
-		try {
-			Thread.sleep(10000);
-			File downloadFolder = new File(System.getProperty("user.home") + "/Downloads");
-			File[] listOfFiles = downloadFolder.listFiles();
-//			ConfigFileReader reader = new ConfigFileReader("configs/config.properties");    	
-			String defaultDir = System.getProperty("user.home") + "/Desktop";
-			
-			Arrays.sort(listOfFiles, new Comparator<File>() {
-				public int compare(File f1, File f2) {
-					return Long.valueOf(f1.lastModified()).compareTo(f2.lastModified());
-				}
-			});
-
-			Integer index = listOfFiles.length - 1;
-			
-			File dest = new File(defaultDir + "/" + listOfFiles[index].getName());
-			
-			FileUtils.copyFile(listOfFiles[index], dest);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
+//	private void saveFile() {
+//		try {
+//			Thread.sleep(10000);
+//			File downloadFolder = new File(System.getProperty("user.home") + "/Downloads");
+//			File[] listOfFiles = downloadFolder.listFiles();
+////			ConfigFileReader reader = new ConfigFileReader("configs/config.properties");    	
+//			String defaultDir = System.getProperty("user.home") + "/Desktop";
+//			
+//			Arrays.sort(listOfFiles, new Comparator<File>() {
+//				public int compare(File f1, File f2) {
+//					return Long.valueOf(f1.lastModified()).compareTo(f2.lastModified());
+//				}
+//			});
+//
+//			Integer index = listOfFiles.length - 1;
+//			
+//			File dest = new File(defaultDir + "/" + listOfFiles[index].getName());
+//			
+//			FileUtils.copyFile(listOfFiles[index], dest);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 	@Before
 	public void init() {
 		driver = new ChromeDriver();
 		home = new HomePage(driver);
 		home.start("https://google.com");
+	}
+	
+	@After
+	public void end() {
+		driver.quit();
 	}
 
 	@Ignore
@@ -83,7 +85,6 @@ public class ExampleTest extends GenerateReport {
 		home.downloadPDF();
 		home.downloads();
 		driver.quit();
-		saveFile();
 	}
 	
 	@Test
